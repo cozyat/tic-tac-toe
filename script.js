bd45b76242894fdad176d6e779e5f4dae8f75b74
Optosis <98789958+cozyat@users.noreply.github.com>
2022-12-28 13:13:20 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function handleDragStart(e) {
+    this.style.opacity = '0.4';
+}
+
+function handleDragEnd(e) {
+    this.style.opacity = '1.1';
+}
+
+let items = document.querySelectorAll('.container .box');
+items.forEach(function (item) {
+    item.addEventListener('dragstart', handleDragStart);
+    item.addEventListener('dragend', handleDragEnd);
+});
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data).cloneNode(true));
+}
