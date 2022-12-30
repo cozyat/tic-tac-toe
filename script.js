e8795d86555a064db726cc72ec299d84f13d6931
cozyat <98789958+cozyat@users.noreply.github.com>
2022-12-30 13:34:36 -0600

    added tic-tac-toe logic for a win

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const box = document.querySelectorAll(".box");
+const statusText = document.querySelector("#statusText");
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let options = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+let running = false;
+
 function handleDragStart(e) {
     this.style.opacity = '0.4';
 }
@@ -14,6 +31,15 @@ items.forEach(function (item) {
 
 function allowDrop(ev) {
     ev.preventDefault();
+
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if (options[cellIndex] != "" || !running) {
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
 }
 
 function drag(ev) {
@@ -26,12 +52,46 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data).cloneNode(true));
 }
 
+function checkWinner() {
+    let roundWon = false;
+
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const boxA = options[condition[0]];
+        const boxB = options[condition[1]];
+        const boxC = options[condition[2]];
+
+        if (boxA == "" || boxB == "" || boxC == "") {
+            continue;
+        }
+
+        if (boxA == boxB && boxB == boxC) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        round_response = alert("The participating player has won!");
+        running = false;
+    } else if (!options.includes("")) {
+        round_response = alert("Draw!");
+        running = false;
+    } else {
+        // Resuming Game
+    }
+}
+
 function resetBoard() {
-    const response = confirm("Are you sure you want to reset?")
-    if (response === true) {
+    const reset_response = confirm("Are you sure you want to reset?")
+    if (reset_response === true) {
         location.reload();
         return true;
     } else {
-        alert("Resuming your game now.")
+        setTimeout(() => {
+            alert("Resuming your game now.");
+        }, 400);
     }
 }
+
+document.getElementById("1")
